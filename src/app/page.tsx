d8e44c3db4e6f4b85dc29261b701d86eb39af4e4
kayy-creator <kaymitchell675@gmail.com>
2025-08-09 03:05:53 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import { Footer } from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background font-body">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1">
         <Hero />
